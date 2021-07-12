d608501b64ff356c105fa6c7ddab0d10e4a1b147
joy-phy <2209572482@qq.com>
2021-07-12 15:42:31 +0800

    7/12 afternoon

--- src/js/detail.js
+++ src/js/detail.js
@@ -7,13 +7,13 @@ $(function () {
 
 
 
-            let count = 3;
+            let count = 0;
             let timers;
             let timer
 
             let int = () => {
 
-                $('.banner.container .wrapper').eq(count).animate({
+                $('.wrapper').eq(count).animate({
                     opacity: 1,
                     // 'z-index':'66'
 
@@ -22,9 +22,9 @@ $(function () {
                     queue: false
                 });
 
-                $('.banner.container .wrapper').eq(count).siblings('.wrapper').css('opacity', '0');
+                $('.wrapper').eq(count).siblings('.wrapper').css('opacity', '0');
                 count++;
-                if (count == 4) {
+                if (count == 6) {
                     count = 0
                 }
                 // $('.banner.container .wrapper').eq(count).animate({
@@ -32,32 +32,31 @@ $(function () {
 
 
                 // }, 1000);
-                $('.remotebanner').children('span').eq(count).addClass('active').siblings().removeClass('active')
-                console.log($('.remotebanner').children('span'))
+                $('.swiper-control').children('span').eq(count).addClass('active').siblings().removeClass('active')
                 return int;
             }
-            timer = setInterval(int(), 4600);
+            timer = setInterval(int(), 8000);
 
-            $('.banner.container').on('mouseleave', function () {
+            $('.image-left.col-6.clearfix').on('mouseleave', function () {
                 timers = setTimeout(() => {
-                    timer = setInterval(int(), 4600);
+                    timer = setInterval(int(), 8000);
                 }, 2000);
 
             })
-            $('.remotebanner').on('click', function (ev) {
+            $('.swiper-control').on('click', function (ev) {
 
                 clearInterval(timer);
                 clearTimeout(timers);
                 $(ev.target).addClass('active').siblings().removeClass('active');
 
                 if ($(ev.target).index() == 0) {
-                    count = 5
+                    count = 7
                 }
                 count = $(ev.target).index() - 1;
                 timers = setTimeout(() => {
-                    timer = setInterval(int(), 4600);
+                    timer = setInterval(int(), 8000);
                 }, 2000);
-                $('.banner.container .wrapper').eq(count).css('opacity', 1).siblings('.wrapper').css('opacity', '0');
+                $('.wrapper').eq(count).css('opacity', 1).siblings('.wrapper').css('opacity', '0');
 
                 //   $('.remotebanner').children('a').eq(count).css('opacity',1).siblings().css('opacity',0)
 
@@ -69,15 +68,15 @@ $(function () {
 
 
 
-            $('footer .linkbar > ul:last-of-type a:last-of-type').mouseenter(function () {
+            $('footer .linkbar > ul:last-of-type a:last-of-type').on('mouseenter',function () {
                 $('footer .linkbar > ul:last-of-type .binacode').css('display', 'block')
-            }).mouseleave(function () {
+            }).on('mouseleave',function () {
                 $('footer .linkbar > ul:last-of-type .binacode').css('display', 'none')
 
             })
 
 
-            $('#banner.container').on('mouseenter', function () {
+            $('.image-left.col-6.clearfix').on('mouseenter', function () {
                 clearTimeout(timers)
                 clearInterval(timer)
             })
@@ -161,9 +160,9 @@ $(function () {
                 clearInterval(timer)
                 count -= 2;
 
-                timer = setInterval(int(), 4600);
+                timer = setInterval(int(), 8000);
                 if (count < 0) {
-                    count = 3
+                    count = 5
                 }
 
                 console.log(count);
@@ -173,7 +172,7 @@ $(function () {
                 clearTimeout(timers)
                 clearInterval(timer)
 
-                timer = setInterval(int(), 4600);
+                timer = setInterval(int(), 8000);
 
 
                 console.log(count);
@@ -189,6 +188,16 @@ $(function () {
 
             })
             $('.fixednav-right').on('mouseenter', function () {
+                $('#banner.container').css({
+                    display: 'block'
+                })
+            }).on('mouseleave', function () {
+                $('#banner.container').css({
+                    display: 'none'
+                })
+
+            })
+            $('.fixednav-right').on('mouseenter', function () {
                 $(this).css({
                     display: 'block'
                 })
@@ -197,23 +206,62 @@ $(function () {
                     display: 'none'
                 })
 
+            })
+            $('.togglenav').on('mouseenter', function () {
+                $('#banner.container').css({
+                    display: 'block'
+                })
+            }).on('mouseleave', function () {
+                $('#banner.container').css({
+                    display: 'none'
+                })
+            });
+            $('.fixednav').on('mouseenter', function () {
+                $('#banner.container').css({
+                    display: 'block'
+                })
+            }).on('mouseleave', function () {
+                $('#banner.container').css({
+                    display: 'none'
+                })
+            })
 
-
-                document.onmousewheel = function () {
-                    if (document.documentElement.scrollTop >= 750) {
-                        $('.removethis').remove();
-                        $(`<div class="removethis" style="margin-top:10px;"><a href="#">
+            document.onmousewheel = function () {
+                if (document.documentElement.scrollTop >= 750) {
+                    $('.removethis').remove();
+                    $(`<div class="removethis" style="margin-top:10px;"><a href="#">
             <div>
                 <span style="transform:rotate(-90deg)" class="iconfont icon-you
 "></span>
                 <i>回到顶部</i>
             </div>
         </a></div>`).appendTo($('aside'))
-                    } else {
-                        $('.removethis').remove();
-
-                    }
+                } else {
+                    $('.removethis').remove();
 
                 }
+
+
+            }
+
+            $('nav .navbar ul.nav li:gt(0)').on('mouseenter', function () {
+                $('nav .navbar ul.nav .navmenu').css({
+                    height: '230px',
+                    transition: 'all .3s',
+                    'box-shadow': ' 0 1px 3px 0 lightgray',
+                    'border-top': ' 1px solid lightgray'
+                })
+            }).on('mouseleave', function () {
+                $('nav .navbar ul.nav .navmenu').css({
+                    height: '0px',
+                    transition: 'all .3s',
+
+                })
+            })
+            $('.text-bottom div:eq(1) button').on('click',function(){
+                $(this).addClass('active').siblings().removeClass('active')
+            })
+            $('.text-bottom div:eq(2) button').on('click',function(){
+                $(this).addClass('active').siblings().removeClass('active')
             })
             })
