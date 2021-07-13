7c58539182b47782668568bdaf66b279ad3e177e
joy-phy <2209572482@qq.com>
2021-07-13 14:49:23 +0800

    7/13 afternoon

--- src/js/register.js
+++ src/js/register.js
@@ -26,7 +26,7 @@ $(function () {
         }, 1000);
     })
 
-    $('.mi-account > .container > .box> p:first-of-type').on('click', function (ev) {
+    $('.mi-account > .container > .box> p:first-of-type').on('click', 'a', function (ev) {
         $(ev.target).addClass('active').siblings().removeClass('active')
 
         if ($(ev.target).index() == 1) {
@@ -52,6 +52,85 @@ $(function () {
             })
 
         }
+    })
+    $('.tab-list').each((index, el) => {
+
+        $(el).children('div:lt(3)').on('click', function (ev) {
+            console.log(ev.target.nodeName)
+
+
+            if (ev.target.nodeName.toLowerCase() == 'label') {
+                $(ev.target).css({
+                    height: '20px',
+                    transition: 'all .2s',
+                    'font-size': '12px',
+                    color:'rgba(0, 0, 0, 0.4)',
+                    padding: `5px 0 0 0`,
+                    'margin': `0 0 0 20px`
+
+                })
+                if ($(this).index() / 2 == 1) {
+                    $(`<input type="password" id="d${$(this).index()/2}" style="font-size:17px; display:block;margin-left:20px ;outline:none;border:none;">`).appendTo($(ev.target).parent())
+                } else {
+
+                    $(`<input type="email" id="d${$(this).index()/2}" style="font-size:17px; display:block;margin-left:20px ;outline:none;border:none;">`).appendTo($(ev.target).parent())
+                }
+                $(ev.target).parent().css({
+                    border: '1px solid orangered ',
+                    backgroundColor: 'white',
+                    boxShadow: `0 0 2px 0 darkorange`
+                })
+            }
+
+            $('.tab-list>div:lt(3)').each((index, el) => {
+                $(el).children('input').on('blur', function () {
+                    if (!$(this).val()) {
+                        $(ev.target).parent().css({
+                            border: 'none',
+                            backgroundColor: '#FCF2F3',
+                            color: 'red',
+                            transition:'all .3s'
+
+                        })
+                        $(ev.target).parent().next('span').css({
+                            display:'block'
+                        })
+                        $(ev.target).css({
+                            height: '100%',
+                            width:"100%",
+                            color:'orangered',
+                            transition: 'all .2s',
+                            'font-size': '17px',
+                            padding: `20px`,
+                            'margin': `0`,
+                            transition:'all .3s'
+
+
+                        })
+                        $(this).remove()
+
+                    }else{
+                        $(ev.target).parent().css({
+                            border: 'none',
+                            boxShadow:'none',
+                            backgroundColor: '#F9F9F9',
+                            transition:'all .3s'
+                        })
+                        $(ev.target).parent().next('span').css({
+                            display:'none'
+                        })
+                        $(ev.target).css({
+                            backgroundColor:'#F9F9F9',
+                            transition:'all .3s'
+
+                        })
+                    }
+                })
+            })
+
+        })
+
+
     })
 
 })
