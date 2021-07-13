2d5a917c3b2991567f2fbeeadefd0c0912b9cb08
joy-phy <2209572482@qq.com>
2021-07-13 10:41:39 +0800

    7/13 noon

--- src/js/register.js
+++ src/js/register.js
@@ -1,34 +1,56 @@
 $(function () {
-    let o1 = {
-        'transform': 'rotate(180deg)',
-        'border-top-color': 'darkorange',
-        transition: 'all .3s'
-    };
-    let o2 = {
-        'transform': 'rotate(0deg)',
-        'border-top-color': '#838383',
-        transition: 'all .3s'
-    }
-    $('.register > div:last-of-type a:last-of-type').on('mouseenter', function () {
-        $('.register > div:last-of-type a:last-of-type span').css(o1)
+    let timer;
+    $('.register > div:last-of-type>a:last-of-type').on('mouseenter', function () {
+        $(this).addClass('active');
+        $('.register > div:last-of-type>ul').css({
+            height: 'auto',
+        })
     }).on('mouseleave', function () {
-        $('.register > div:last-of-type a:last-of-type span').css(o2)
+        timer = setTimeout(() => {
+            $(this).removeClass('active');
+            $('.register > div:last-of-type>ul').css({
+                height: '0',
+            })
+        }, 1000);
+
+
+    })
+    $('.register > div:last-of-type>ul').on('mouseenter', function () {
+        clearTimeout(timer);
+    }).on('mouseleave', function () {
+        timer = setTimeout(() => {
+            $('.register > div:last-of-type>a:last-of-type').removeClass('active');
+            $('.register > div:last-of-type>ul').css({
+                height: '0',
+            })
+        }, 1000);
     })
 
-    $('.mi-account > .container > .box > .tab-list > p:first-of-type').on('click', function (ev) {
+    $('.mi-account > .container > .box> p:first-of-type').on('click', function (ev) {
         $(ev.target).addClass('active').siblings().removeClass('active')
 
         if ($(ev.target).index() == 1) {
-            $('.mi-account > .container > .box > .tab-list > p:first-of-type i').css({
-                transform:'translateX(0px)',
-                transition:'all .3s'
+            $('.mi-account > .container > .box> p:first-of-type i').css({
+                transform: 'translateX(0px)',
+                transition: 'all .3s'
             })
-            
-        }else{
-             $('.mi-account > .container > .box > .tab-list > p:first-of-type i').css({
-                transform:'translateX(68px)',
-                transition:'all .3s'
+            console.log($('.controlbar'))
+            $('.controlbar').css({
+                transform: 'translateX(0)',
+                transition: 'all .3s'
+
             })
+        } else {
+            $('.mi-account > .container > .box> p:first-of-type i').css({
+                transform: 'translateX(68px)',
+                transition: 'all .3s'
+            })
+            $('.controlbar').css({
+                transform: 'translateX(-414px)',
+                transition: 'all .3s'
+
+            })
+
         }
     })
 
